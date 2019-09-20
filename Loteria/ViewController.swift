9abeeb4fc2812f4bae26be5b394732ae1b23aac0
Diego Costa Pereira <dspereira@faap.br>
2019-09-20 15:11:14 -0300

    numeros aleatorios para fazer jogos de loteria e quina

--- Loteria/ViewController.swift
+++ Loteria/ViewController.swift
@@ -1,9 +1,21 @@
 import UIKit
 
-//enum GameType: String{
-//    case megasena = "Mega-Sena"
-//    case quina = "Quina"
-//}
+enum GameType: String{
+    case megasena = "Mega-Sena"
+    case quina = "Quina"
+}
+
+infix operator >--<
+func >--< (total: Int, universe: Int) -> [Int]{
+    var result: [Int] = []
+    while result.count < total {
+        let randonNumber = Int(arc4random_uniform(UInt32(universe))+1)
+        if !result.contains(randonNumber){
+            result.append(randonNumber)
+        }
+    }
+    return result.sorted()
+}
 
 class ViewController: UIViewController {
 
@@ -14,15 +26,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        
+        showNumbers(type: .megasena)
     }
     
     func showNumbers(type: GameType){
+        lbGameType.text = type.rawValue
+        var game: [Int] = []
         
+        switch type {
+        case .megasena:
+            game = 6>--<60
+            balls.last?.isHidden = false
+        case .quina:
+            game = 5>--<80
+            balls.last?.isHidden = true
+        }
+        
+        for (index, game)in game.enumerated(){
+            balls[index].setTitle("\(game)", for: .normal)
+        }
     }
     
     @IBAction func generateGame() {
-    
+        switch scGameType.selectedSegmentIndex {
+        case 0:
+            showNumbers(type: .megasena)
+        default:
+            showNumbers(type: .quina)
+        }
     }
 }
 
